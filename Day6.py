a1fd6740a2c74455c84571e464c60b4c7c4f917e
Nebsorg <75315494+Nebsorg@users.noreply.github.com>
2021-12-06 10:53:56 +0100

    Add files via upload

--- Day6.py
+++ Day6.py
@@ -1,3 +1,4 @@
+import copy
 from datetime import datetime
 from collections import defaultdict
 
@@ -6,29 +7,31 @@ print("--- Day 6: Lanternfish ---")
 def naissance(couveuse, numberOfDays, caption):
 
     for i in range(numberOfDays):
-        naissances = defaultdict(lambda: 0)
-        for day in range(1,9):
-            naissances[day-1] = couveuse[day]
-        naissances[6] += couveuse[0]
-        naissances[8] += couveuse[0]
-
-        couveuse = naissances
+        newFish = couveuse[0]
+        for day in range(8):
+            couveuse[day] = couveuse[day+1]
+        couveuse[6] += newFish
+        couveuse[8] = newFish
 
     numberOfFishs = sum(couveuse.values())
     print(f"  ** {caption} : Number of fish after {numberOfDays} days = {numberOfFishs}")
     return(numberOfFishs)
 
 start_time = datetime.now()
-couveuse = defaultdict(lambda: 0)
+instructions = defaultdict(lambda: 0)
 
 f = open(".\Day6.txt", "r")
 for line in f:
     instruction = line.rstrip().split(",")
     for v in instruction:
-        couveuse[int(v)] += 1
+        instructions[int(v)] += 1
 f.close()
 
+
+couveuse = copy.deepcopy(instructions)
 nbOfFish = naissance(couveuse, 80, "Star 1")
+
+couveuse = copy.deepcopy(instructions)
 nbOfFish = naissance(couveuse, 256, "Star 2")
 
 end_time = datetime.now()
